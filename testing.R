b8aa302bc62bb4ef45ca96e9eb00d4611ff23bcc
PiotrTymoszuk <80723424+PiotrTymoszuk@users.noreply.github.com>
2023-03-15 18:53:18 +0100

    GitHub examples

--- testing.R
+++ testing.R
@@ -1,32 +1,27 @@
 # tools -----
 
   library(trafo)
+  library(plyr)
+  library(tidyverse)
 
 # Example data ------
 
-  my_cars <- tibble::rownames_to_column(mtcars, 'cars')
+  ## example data: tibble, data frame and matrix
 
-  my_cars2 <-
-    dplyr::mutate(mtcars,
-                  ncap = sample(c(1:10, NA), nrow(mtcars), replace = TRUE))
+  my_cars <- mtcars %>%
+    rownames_to_column('cars') %>%
+    select(- vs, - am) %>%
+    as_tibble
 
-  my_cars <- dplyr::select(my_cars2, -vs, -am)
+  my_cars2 <- mtcars %>%
+    mutate(mtcars,
+           ncap = sample(c(1:10, NA),
+                         nrow(mtcars),
+                         replace = TRUE))
 
-  my_cyl_mtx <- matrix(sample(1:8, 20, replace = TRUE), nrow = 2, ncol = 10)
-
-  cyl_reco <- tibble::tibble(cyl = c(4, 6, 8, 10, 12),
-                             cyl_lab = c('4-cylinder',
-                                         '6-cylinder',
-                                         '6+ cylinder',
-                                         '6+-cylinder',
-                                         '6+-cylinder'))
-
-  my_cyl_list <- list('A' = sample(1:8, 20, replace = TRUE),
-                      'B' = sample(1:8, 20, replace = TRUE),
-                      'C' = matrix(c(12, 4, 8, 10),
-                                   c(6, 4, 8, 10),
-                                   ncol = 2),
-                      'D' = my_cars)
+  my_cyl_mtx <- matrix(sample(1:8, 20, replace = TRUE),
+                       nrow = 2,
+                       ncol = 10)
 
   my_mtx <- matrix(rnorm(100),
                    nrow = 20,
@@ -35,6 +30,25 @@
   my_mtx2 <- matrix(rnorm(100),
                     nrow = 5,
                     ncol = 20)
+
+
+  my_cyl_list <- list('A' = sample(1:8, 20, replace = TRUE),
+                      'B' = sample(1:8, 20, replace = TRUE),
+                      'C' = matrix(c(12, 4, 8, 10),
+                                   c(6, 4, 8, 10),
+                                   ncol = 2),
+                      'D' = my_cars)
+
+  ## and a dictionary mapping the cylinder number to a text label
+
+
+  cyl_reco <- tibble::tibble(cyl = c(4, 6, 8, 10, 12),
+                             cyl_lab = c('4-cylinder',
+                                         '6-cylinder',
+                                         '6+ cylinder',
+                                         '6+-cylinder',
+                                         '6+-cylinder'))
+
 
 # Exchanging -------
 
@@ -89,10 +103,19 @@
            values_to = 'lst_elements',
            simplify = TRUE)
 
-  compress(plyr::dlply(mtcars,
-                       'cyl',
-                       tibble::rownames_to_column,
-                       'car'))
+    my_cars %>%
+      dlply('cyl') %>%
+      compress(names_to = 'cyl') %>%
+      as_tibble
+
+    c('4' = '4-cylinder',
+      '6' = '6-cylinder',
+      '8' = '6+ cylinder',
+      '10' = '6+-cylinder',
+      '12' = '6+-cylinder') %>%
+      compress(names_to = 'cyclinder_number',
+               values_to = 'cylinder_category')
+
 
 # Binding ----
 
@@ -115,10 +138,9 @@
 
   ## inner
 
-  inner_rbind(tibble::as_tibble(my_cars),
-              tibble::as_tibble(my_cars2))
+  inner_rbind(my_cars, my_cars2)
 
-  inner_rbind(my_cars[1:3], my_cars[5:8])
+  inner_rbind(my_cars[1:3], my_cars[6:7])
 
   inner_rbind(my_mtx, my_mtx2)
 
@@ -135,16 +157,20 @@
 
 # Splitting ---------
 
-  blast(tibble::as_tibble(my_cars),
-        dplyr::all_of(c('cyl', 'gear')),
-        carb)
+  my_cars %>%
+    blast(all_of(c('cyl', 'gear')),
+          carb)
 
   blast(my_cars, c('cyl', 'gear'))
 
 # Regular expression search -----
 
-  reglook(tibble::rownames_to_column(my_cars, 'model'),
+  reglook(my_cars,
           regex = '^(Merc|Honda)')
+
+  reglook(my_cars,
+          regex = '4|6',
+          keys = 'cyl')
 
 # Complete observation series ------
 
